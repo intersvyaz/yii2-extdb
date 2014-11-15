a495b583670739ade5047f42ace9b2e008969b77
miramir <gmiramir@gmail.com>
2014-11-15 11:19:29 +0500

    перевёл на использование intersvyaz/yii2-sqlparser

--- Command.php
+++ Command.php
@@ -1,6 +1,7 @@
 <?php
 namespace Intersvyaz\ExtDb;
 
+use Intersvyaz\SqlParser\Parser;
 use Yii;
 
 /**
@@ -29,10 +30,7 @@ class Command extends \yii\db\Command
     public function setSql($sql, array $params = [])
     {
         if (!empty($sql)) {
-            if (substr($sql, -4) === '.sql')
-                $sql = file_get_contents($sql);
-
-            $sql = $this->prepareSql($sql, $params);
+            $sql = (new Parser($sql, $params))->getSql();
         }
 
         return parent::setSql($sql);
@@ -45,109 +43,6 @@ class Command extends \yii\db\Command
      */
     public function bindValues($values)
     {
-        $values = $this->simplifyParams($values);
-        return parent::bindValues($values);
-    }
-
-    /**
-     * Функция разбора и подготовки текста sql запроса.
-     * @param  string $sql Запрос который нужно подготовить.
-     * @param array $params Параметры построения запроса.
-     * @return string Готовый текст sql запроса.
-     */
-    protected function prepareSql($sql, array &$params = [])
-    {
-        // Разбор многострочных комментариев
-        if (preg_match_all('#/\*(\w+)(.+?)\*/#s', $sql, $matches)) {
-            $count = count($matches[0]);
-            for ($i = 0; $i < $count; $i++) {
-                $sql = $this->replaceComment($sql, $matches[0][$i], $matches[2][$i], $matches[1][$i], $params);
-            }
-        }
-
-        // Многоитерационный разбор однострчных комментариев
-        while (true) {
-            if (preg_match_all('#--\*(\w+)(.+)#', $sql, $matches)) {
-                $count = count($matches[0]);
-                for ($i = 0; $i < $count; $i++) {
-                    $sql = $this->replaceComment($sql, $matches[0][$i], $matches[2][$i], $matches[1][$i], $params);
-                }
-            } else {
-                break;
-            }
-        }
-
-        return preg_replace("/\n+/", "\n", trim($sql));
-    }
-
-    /**
-     * Заменяем коментарий в запросе на соответствующе преобразованный блок или удаляем.
-     * @param string $query Текст запроса.
-     * @param string $comment Заменямый комментарий.
-     * @param string $queryInComment Текст внутри комментария.
-     * @param string $paramName Имя параметра.
-     * @param array $params Массив параметров
-     * @return string Запрос с замененным комментирием.
-     */
-    protected function replaceComment($query, $comment, $queryInComment, $paramName, array &$params)
-    {
-		$paramName = ltrim($paramName,':');
-        if(array_key_exists($paramName, $params)) {
-            $paramValue = $params[$paramName];
-            if (is_array($paramValue)) {
-                $value = isset($paramValue[0]) ? $paramValue[0] : null;
-                $bind = isset($paramValue['bind']) ? $paramValue['bind'] : true;
-            } else {
-                $value = $paramValue;
-                $bind = true;
-            }
-
-            if ($bind === true && is_array($value)) {
-                $valArr = [];
-                foreach (array_keys($value) as $keyVal) {
-                    $valArr[] = ':' . $paramName . '_' . $keyVal;
-                }
-                $replacement = implode(',', $valArr);
-                $queryInComment = preg_replace('/:@' . preg_quote($paramName) . '/', $replacement, $queryInComment);
-            } elseif ($bind === 'text') {
-                $queryInComment = preg_replace('/' . preg_quote($paramName) . '/', $value, $queryInComment);
-            }
-        }else{
-            $queryInComment = '';
-        }
-
-        return str_replace($comment, $queryInComment, $query);
-    }
-
-    /**
-     * Конвертирует параметры запроса из расширенного формата в параметры пригодные для \yii\db\Command::bindValues.
-     * @param array $params Параметры построения запроса.
-     * @return array
-     */
-    protected function simplifyParams($params)
-    {
-        if (empty($params)) {
-            return $params;
-        }
-
-        $newParams = [];
-        foreach ($params as $key => $value) {
-			$key = ':' . ltrim($key,':');
-            if (is_array($value)) {
-                if (isset($value[0]) && is_array($value[0])) {
-                    foreach ($value[0] as $valKey => $valVal)
-                        $newParams[$key . '_' . $valKey] = $valVal;
-                } elseif (!isset($value['bind']) || $value['bind'] === true) {
-                    if(isset($value[0]) && isset($value[1]))
-                        $newParams[$key] = [$value[0],$value[1]];
-                    elseif(isset($value[0]))
-                        $newParams[$key] = $value[0];
-                }
-            } else {
-                $newParams[$key] = $value;
-            }
-        }
-
-        return $newParams;
+        return parent::bindValues((new Parser('', $values))->getSimplifiedParams());
     }
 }
